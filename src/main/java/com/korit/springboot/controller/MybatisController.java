274b162684cbfad8a85fe07fea95ff0d641fff2f
gyeonggumun <lion989072@gmail.com>
2025-12-04 15:36:38 +0900

    입력 받아서 insert DTO활용 정리

--- src/main/java/com/korit/springboot/controller/MybatisController.java
+++ src/main/java/com/korit/springboot/controller/MybatisController.java
@@ -1,10 +1,14 @@
 package com.korit.springboot.controller;
 
+import com.korit.springboot.dto.InsertDto;
 import com.korit.springboot.mapper.StudyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class MybatisController {
@@ -12,9 +16,21 @@ public class MybatisController {
     @Autowired    //
     private StudyMapper studyMapper;
 
+//    @PostMapping("/mybatis/study")
+//    public ResponseEntity<?> insert(String name, int age) {
+//        studyMapper.insert(name, age);
+//        return ResponseEntity.ok().build();
+//    }
+
+//    @PostMapping("/mybatis/study")
+//    public  ResponseEntity<?> insert(@RequestBody Map<String, Object> reqMap) {
+//        studyMapper.insert((String) reqMap.get("name"), (Integer) reqMap.get("age"));
+//        return ResponseEntity.ok().build();
+//    }
+
     @PostMapping("/mybatis/study")
-    public ResponseEntity<?> insert() {
-        studyMapper.insert("문경구", 25);
+    public  ResponseEntity<?> insert(InsertDto insertDto) {
+        studyMapper.insert(insertDto.getName(), insertDto.getAge());
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/korit/springboot/dto/InsertDto.java
@@ -0,0 +1,9 @@
+package com.korit.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class InsertDto {
+    private String name;
+    private int age;
+}
