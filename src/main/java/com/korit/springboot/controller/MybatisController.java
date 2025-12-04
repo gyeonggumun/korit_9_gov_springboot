47fd8c6ec20e8e8a68b7571bdcc3d4c56374ea73
gyeonggumun <lion989072@gmail.com>
2025-12-04 17:42:27 +0900

    select 연습

--- src/main/java/com/korit/springboot/controller/MybatisController.java
+++ src/main/java/com/korit/springboot/controller/MybatisController.java
@@ -6,9 +6,14 @@ import com.korit.springboot.mapper.ProductMapper;
 import com.korit.springboot.mapper.StudyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.ls.LSInput;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class MybatisController {
@@ -40,5 +45,11 @@ public class MybatisController {
     public ResponseEntity<?> productInsert(@RequestBody RequestInsertProductDto dto) {
         priceMapper.insert(dto.getProductName(), dto.getSize(), dto.getPrice());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/mybatis/study")
+    public ResponseEntity<?> selectName() {
+        List<Map<String, Object>> foundName = studyMapper.findAllName();
+        return ResponseEntity.ok(foundName);
     }
 }
--- src/main/java/com/korit/springboot/mapper/StudyMapper.java
+++ src/main/java/com/korit/springboot/mapper/StudyMapper.java
@@ -4,9 +4,10 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface StudyMapper {
     int insert(@Param("name") String name,@Param("age") int age);
-    List<String> findAllName();   // 객체일 때를 제외하고는 resultType을 제외하고 사용가능
+    List<Map<String, Object>> findAllName();   // 객체일 때를 제외하고는 resultType을 제외하고 사용가능
 }
