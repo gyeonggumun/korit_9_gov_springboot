f2ebec10ae6bb2b43ac47b266a2fa164241d1f99
gyeonggumun <lion989072@gmail.com>
2025-12-06 01:59:31 +0900

    cosmetics 이름찾 중복검사

--- src/main/java/com/korit/springboot/mapper/CosmeticsMapper.java
+++ src/main/java/com/korit/springboot/mapper/CosmeticsMapper.java
@@ -2,8 +2,10 @@ package com.korit.springboot.mapper;
 
 import com.korit.springboot.entity.CosmeticsEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface CosmeticsMapper {
     int insert(CosmeticsEntity cosmeticsEntity);
+    CosmeticsEntity findCosmeticsByCosmeticsName(@Param("cosmetics_name") String cosmeticsName);
 }
