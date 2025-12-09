2c07b32e05a788fef0cd8ef17a416473cd9a7b63
gyeonggumun <lion989072@gmail.com>
2025-12-09 10:14:22 +0900

    jwt 기본설정

--- src/main/java/com/korit/springboot/jwt/JwtTokenProvider.java
+++ src/main/java/com/korit/springboot/jwt/JwtTokenProvider.java
@@ -1,7 +1,41 @@
 package com.korit.springboot.jwt;
 
+import com.korit.springboot.entity.UserEntity;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+
+    private final SecretKey key;
+
+    // @value는 yml의 값을 들고와서 사용할 때 씀
+    public JwtTokenProvider(@Value("${jwt.secret}") String secret) { // jwt에 맞는 key라고 하는 객체생성
+        key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // 토큰을 만들기 위해 user정보가 필요해서 매개변수로 UserEntity를 받아옴
+    public String createAccessToken(UserEntity userEntity) {
+        Date now = new Date();  // 현재시간
+        Long expiredTime = now.getTime() + (1000l * 60l * 60l * 24l); // 현시점에서 24시간 증가
+        Date expiredDate = new Date(expiredTime);   // Date로 타입 변환 new 되는 시점 부터 24시간
+
+
+        return Jwts.builder()
+                .subject("Server Access Token") // 제목
+                .issuer("문경구") // 이름
+                .issuedAt(new Date()) // 현시간 설정
+                // 위 3개는 없어도 괜찮지만 아래 3개는 필수로 필요한 항목
+                .expiration(expiredDate) // 지속시간
+                .claim("userId", userEntity.getUserId())  // 몇번째유저인지
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
