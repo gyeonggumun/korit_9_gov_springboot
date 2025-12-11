fdb35f66a78cd1a01c9d90b7e6a16a374ab59a26
gyeonggumun <lion989072@gmail.com>
2025-12-11 16:56:56 +0900

    프론트연결

--- src/main/java/com/korit/springboot/controller/SecurityRequirementController.java
+++ src/main/java/com/korit/springboot/controller/SecurityRequirementController.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SecurityRequirementController {
@@ -23,5 +26,11 @@ public class SecurityRequirementController {
         System.out.println(principalUser2.getUserId());
         System.out.println(principalUser2.getUserEntity().getName());
         return ResponseEntity.ok("응답");
+    }
+
+    @GetMapping("/api/security/data")
+    public ResponseEntity<String> getData(@RequestParam int index) {
+        List<String> data = List.of("a", "b", "c", "d");
+        return ResponseEntity.ok(data.get(index));
     }
 }
